d2c2322da7624cd462a95e3b36f0f85401974ebf
noelcv <noelguiavieira@gmail.com>
2023-07-22 19:54:11 +0200

    csp

--- server/index.ts
+++ server/index.ts
@@ -34,7 +34,10 @@ app.use(morgan('dev'));
 app.use(Express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://whois-monorepo-client.vercel.app'
+  );
   next();
 });
 app.use(router);
