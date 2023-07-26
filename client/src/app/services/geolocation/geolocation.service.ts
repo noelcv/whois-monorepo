511260838788890fcabad366116cf5b3a0e7a676
noelcv <noelguiavieira@gmail.com>
2023-07-26 16:19:28 +0200

    feat: GeolocationService methods

--- client/src/app/services/geolocation/geolocation.service.ts
+++ client/src/app/services/geolocation/geolocation.service.ts
@@ -6,5 +6,23 @@ import { Injectable } from '@angular/core';
 export class GeolocationService {
   constructor() {}
 
-  static getLocation() {}
+  private getUserLocation() {
+    const success = (position: any): void => {
+      return position;
+    };
+
+    function err() {
+      return err;
+    }
+    navigator.geolocation.getCurrentPosition(success, err);
+  }
+
+  private parseLocation() {
+    const location = this.getUserLocation();
+    return JSON.stringify(location, null, 2);
+  }
+
+  public init() {
+    return this.parseLocation();
+  }
 }
