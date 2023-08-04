a3618ae38a44496dababb676fbe3732d564d030a
noelcv <noelguiavieira@gmail.com>
2023-08-05 00:15:08 +0200

    WQBE: getLocation

--- server/controllers/location.controller.ts
+++ server/controllers/location.controller.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from 'express';
+import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 const LOCATION_API_URL = `${process.env.LOCATION_API_URL}${process.env.LOCATION_TOKEN}`;
 
-interface Location {
+interface LocationCoords {
   latitude: string;
   longitude: string;
+}
+
+interface UserLocation {
+  town: string;
+  country: string;
 }
 
 export async function lookUp(req: Request, res: Response) {
@@ -17,15 +23,24 @@ export async function lookUp(req: Request, res: Response) {
     const long = req.body.longitude as string;
     console.log('lat from Client: ', lat);
     console.log('long from Client: ', long);
-    getLocation(lat, long);
-    const result = JSON.stringify({ lat, long });
-    res.send(result);
+    const userLocation = getLocation(lat, long);
+    res.send(userLocation);
   } catch (err) {
     console.log('❌ Error in location.controller in lookUp: ', err);
   }
 }
 
-const getLocation = (lat: string, long: string) => {
-  const locationQuery = `${LOCATION_API_URL}&lat=${lat}&lon=${long}`;
-  fetch(locationQuery).then;
+const getLocation = async (
+  lat: string,
+  long: string
+): Promise<UserLocation> => {
+  const locationQuery = `${LOCATION_API_URL}&lat=${lat}&lon=${long}&format=json`;
+  const apiResponse = await axios.get(locationQuery);
+  console.log('response from api', apiResponse.data);
+  // const data = JSON.stringify(apiResponse.data, null, 2);
+  // console.log('stringified', data);
+  const town = apiResponse.data.address.town;
+  const country = apiResponse.data.address.country;
+  const userLocation: UserLocation = { town: town, country: country };
+  return userLocation;
 };
