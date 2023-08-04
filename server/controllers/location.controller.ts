6383b1396d901b9bc32b0b87cda21c1c560a3bb3
noelcv <noelguiavieira@gmail.com>
2023-08-05 00:31:10 +0200

    WQBE: feat: location.controller.lookup sending location to client

--- server/controllers/location.controller.ts
+++ server/controllers/location.controller.ts
@@ -16,15 +16,12 @@ interface UserLocation {
 
 export async function lookUp(req: Request, res: Response) {
   try {
-    console.log('req.body from Client: ', req.body);
     if (!req.body.latitude || !req.body.longitude)
       return res.status(400).send('❌ Invalid location data');
     const lat = req.body.latitude as string;
     const long = req.body.longitude as string;
-    console.log('lat from Client: ', lat);
-    console.log('long from Client: ', long);
-    const userLocation = getLocation(lat, long);
-    res.send(userLocation);
+    const userLocation = await getLocation(lat, long);
+    if (userLocation) res.send(userLocation);
   } catch (err) {
     console.log('❌ Error in location.controller in lookUp: ', err);
   }
@@ -33,14 +30,24 @@ export async function lookUp(req: Request, res: Response) {
 const getLocation = async (
   lat: string,
   long: string
-): Promise<UserLocation> => {
-  const locationQuery = `${LOCATION_API_URL}&lat=${lat}&lon=${long}&format=json`;
-  const apiResponse = await axios.get(locationQuery);
-  console.log('response from api', apiResponse.data);
-  // const data = JSON.stringify(apiResponse.data, null, 2);
-  // console.log('stringified', data);
-  const town = apiResponse.data.address.town;
-  const country = apiResponse.data.address.country;
-  const userLocation: UserLocation = { town: town, country: country };
-  return userLocation;
+): Promise<string | undefined> => {
+  try {
+    const locationQuery = `${LOCATION_API_URL}&lat=${lat}&lon=${long}&format=json`;
+    const apiResponse = await axios.get(locationQuery);
+    console.log('response from api', apiResponse.data);
+    if (apiResponse.data) {
+      const town = apiResponse.data.address.town;
+      const country = apiResponse.data.address.country;
+      const location: UserLocation = { town: town, country: country };
+      const userLocation = parseUserLocation(location);
+      console.log(' parsedUserLocation', userLocation);
+      return userLocation;
+    }
+  } catch (err) {
+    console.log('❌ Error at getLocation: ', err);
+  }
+};
+
+const parseUserLocation = (userLocation: UserLocation): string => {
+  return JSON.stringify(userLocation);
 };
