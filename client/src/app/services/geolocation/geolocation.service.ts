6075448bd7bd4f73286742abd2cda2d6a94eaedd
noelcv <noelguiavieira@gmail.com>
2023-08-05 00:33:30 +0200

    WQFE: chore: rename ILocation to ILocationCoords

--- client/src/app/services/geolocation/geolocation.service.ts
+++ client/src/app/services/geolocation/geolocation.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject, map, catchError, Observable, throwError, retry } from 'rxjs';
+import { Subject, catchError, Observable, throwError, retry } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
-export interface ILocation {
+export interface ILocationCoords {
   latitude: string;
   longitude: string;
 }
@@ -12,13 +12,13 @@ export interface ILocation {
 })
 export class GeolocationService {
   private BASE_URL = environment.baseUrl;
-  location: ILocation = { latitude: '0', longitude: '0' };
+  location: ILocationCoords = { latitude: '0', longitude: '0' };
   locationObservable = new Subject();
   constructor(private http: HttpClient) {}
 
   private getUserLocation(): void {
     const success = (position: any): void => {
-      const location: ILocation = {
+      const location: ILocationCoords = {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
       };
@@ -49,7 +49,7 @@ export class GeolocationService {
     this.getUserLocation();
   }
 
-  private geoLookUp(location: ILocation): Observable<any> {
+  private geoLookUp(location: ILocationCoords): Observable<any> {
     return this.http
       .post(`${this.BASE_URL}/location`, location)
       .pipe(retry(3), catchError(this.handleError));
