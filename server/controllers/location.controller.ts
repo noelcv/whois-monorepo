d9eff23afd621cac4427b49b7df3512f4231fe38
noelcv <noelguiavieira@gmail.com>
2023-08-04 21:57:36 +0200

    WQ: chore: close the loop between server and client

--- server/controllers/location.controller.ts
+++ server/controllers/location.controller.ts
@@ -2,13 +2,15 @@ import { Request, Response } from 'express';
 
 export async function lookUp(req: Request, res: Response) {
   try {
-    if (!req.body.lat || !req.body.long)
+    console.log('req.body from Client: ', req.body);
+    if (!req.body.latitude || !req.body.longitude)
       return res.status(400).send('❌ Invalid location data');
-    const lat = req.body.lat as string;
-    const long = req.body.long as string;
+    const lat = req.body.latitude as string;
+    const long = req.body.longitude as string;
     console.log('lat from Client: ', lat);
     console.log('long from Client: ', long);
-    res.send('hello from location controller');
+    const result = JSON.stringify({ lat, long });
+    res.send(result);
   } catch (err) {
     console.log('❌ Error in location.controller in lookUp: ', err);
   }
