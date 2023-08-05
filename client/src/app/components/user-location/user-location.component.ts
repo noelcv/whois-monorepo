38c248fdc1385b646b95c71575bb16fc32015069
noelcv <noelguiavieira@gmail.com>
2023-08-05 14:02:14 +0200

    WQFE: chore: scaffold user-location component

---
+++ client/src/app/components/user-location/user-location.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-location',
+  templateUrl: './user-location.component.html',
+  styleUrls: ['./user-location.component.css'],
+  standalone: true,
+})
+export class UserLocationComponent {
+  something: undefined;
+}
