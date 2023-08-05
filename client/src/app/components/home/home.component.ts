7361316436de600f9ff7e2704fd0f94c27e9d758
noelcv <noelguiavieira@gmail.com>
2023-08-05 14:10:30 +0200

    WQFE: chore: extract logic from Home to  user-location component

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,35 +1,28 @@
-import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { IUserLocation } from 'src/app/services/geolocation/geolocation.interface';
-import { GeolocationService } from 'src/app/services/geolocation/geolocation.service';
 import { HeaderComponent } from '../header/header.component';
 import { SearchbarComponent } from '../search-bar/search-bar.component';
 import { SearchResultsComponent } from '../search-results/search-results.component';
 import { WatchListComponent } from '../watch-list/watch-list.component';
+import { UserLocationComponent } from '../user-location/user-location.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   standalone: true,
+  styleUrls: [],
   imports: [
     HeaderComponent,
     WatchListComponent,
     SearchbarComponent,
     SearchResultsComponent,
-    NgIf,
+    UserLocationComponent,
   ],
-  styleUrls: [],
 })
 export class HomeComponent implements OnInit {
   location$: IUserLocation | undefined;
 
-  constructor(geo: GeolocationService) {
-    geo.init();
-
-    geo.locationObservable.subscribe((location: unknown) => {
-      this.location$ = location as unknown as IUserLocation;
-    });
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
--- client/src/app/components/user-location/user-location.component.ts
+++ client/src/app/components/user-location/user-location.component.ts
@@ -1,11 +1,23 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { IUserLocation } from 'src/app/services/geolocation/geolocation.interface';
+import { GeolocationService } from 'src/app/services/geolocation/geolocation.service';
 
 @Component({
   selector: 'app-user-location',
   templateUrl: './user-location.component.html',
   styleUrls: ['./user-location.component.css'],
+  imports: [NgIf],
   standalone: true,
 })
 export class UserLocationComponent {
-  something: undefined;
+  location$: IUserLocation | undefined;
+
+  constructor(geo: GeolocationService) {
+    geo.init();
+
+    geo.locationObservable.subscribe((location: unknown) => {
+      this.location$ = location as unknown as IUserLocation;
+    });
+  }
 }
